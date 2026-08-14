470608d6086a560fb20fb88791f4b2d471395bd0
Thenateraider <Ekaluck.musik@gmail.com>
2026-08-14 13:45:02 +0700

    Refactor components to use destructured parameters for props in Castle, Tower, Chamber, Room, Hall, Corridor, Gallery, Nook, and SecretRoom

--- src/components/07Gallery.jsx
+++ src/components/07Gallery.jsx
@@ -1,11 +1,11 @@
 import Nook from "./08Nook.jsx";
 
-export default function Gallery(gallery) {
+export default function Gallery({ textFromInput, onSecretRoomChange }) {
   return (
     <div className="w-full h-full bg-violet-500 flex items-center justify-center p-8">
       <div className="w-full h-full flex flex-col items-center justify-center">
         <h2 className="text-white text-xs font-bold mb-8">Gallery</h2>
-        <Nook textFromInput={gallery.textFromInput} onSecretRoomChange={gallery.onSecretRoomChange}/>
+        <Nook textFromInput={textFromInput} onSecretRoomChange={onSecretRoomChange}/>
       </div>
     </div>
   );
--- src/components/09SecretRoom.jsx
+++ src/components/09SecretRoom.jsx
@@ -2,14 +2,14 @@
 import InputBox from "./InputBox";
 import { useState } from "react";
 
-export default function SecretRoom(secretRoom) {
+export default function SecretRoom({ textFromInput, onSecretRoomChange }) {
   const [inputTextFromSecretRoom, setInputTextFromSecretRoom] = useState("");
   const handleChange = (e) => {
     const newValue = e.target.value;
     setInputTextFromSecretRoom(newValue);
 
-    if (secretRoom.onSecretRoomChange) {
-      secretRoom.onSecretRoomChange(newValue);
+    if (onSecretRoomChange) {
+      onSecretRoomChange(newValue);
     }
   };
   return (
@@ -17,7 +17,7 @@ export default function SecretRoom(secretRoom) {
       <div className="w-full h-full flex flex-col items-center justify-center">
         <h2 className="text-white text-sm font-bold mb-4">Secret Room </h2>
         <p className="text-yellow-300 font-bold my-4 text-2xl ">
-          Outside said: {secretRoom.textFromInput ? secretRoom.textFromInput : " ⏳ Waiting for a message from Outside..."}
+          Outside said: {textFromInput ? textFromInput : " ⏳ Waiting for a message from Outside..."}
         </p>
         <InputBox
           value={inputTextFromSecretRoom}
